1b9b8ca0fbc135580ae16db148857394f471e5d9
elmerson15 <94423727+elmerson15@users.noreply.github.com>
2025-05-12 11:53:26 +0530

    Update agent.py

--- bible_agent/agent.py
+++ bible_agent/agent.py
@@ -1,27 +1,30 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
-from browser_use import Agent
+from browser_use import Agent as Agent1
+# from browser_use.logging_config import setup_logging
 import asyncio
 import os
 from dotenv import load_dotenv
-import logging
-from google.adk.agents import Agent
+from google.adk.agents import Agent as Agent2
+# import logging
+# from google.adk.tools import google_search
 
 # Setup Logging
-logging.getLogger("browser_use").setLevel(logging.DEBUG)
+# logging.getLogger("browser_use").setLevel(logging.DEBUG)
+# setup_logging()
 load_dotenv()
 
-async def main() -> dict:
+async def main():
     # Get task prompt from the user
     user_prompt = input()
 
     # Initialize Gemini API
     llm = ChatGoogleGenerativeAI(
         model="gemini-2.0-flash",
-        google_api_key=os.getenv("GOOGLE_API_KEY")
+        google_api_key=os.getenv("GEMINI_API_KEY")
     )
 
     # Create the agent with the raw user prompt
-    agent = Agent(
+    agent = Agent1(
         task=user_prompt,
         llm=llm
     )
@@ -29,10 +32,9 @@ if __name__ == "__main__":
     asyncio.run(main())
 
 
-root_agent = Agent(
+root_agent = Agent2(
     name="bible_agent",
     model="gemini-2.0-flash",
-    instruction=""" go to the main() and in the user_prompt give the query and  """,
+    instruction=""" Get the prompt from the user and send it to main() then the browsr_use automate the process  """,
     tools=[main],
-    output_key="generated_answer",  # Save result to state
 )
